640b8029485b5cecf85142f757669b4298470f93
Sean Perry <shperry@ucsd.edu>
2026-05-22 14:34:42 -0700

    fix(krg-vault) get tls working

--- terraform/openbao/variables.tf
+++ terraform/openbao/variables.tf
@@ -1,5 +1,5 @@
 variable "vault_addr" {
   description = "OpenBao API address"
   type        = string
-  default     = "http://krg-vault.ucsd.edu:8200"
+  default     = "https://krg-vault.ucsd.edu:8200"
 }
