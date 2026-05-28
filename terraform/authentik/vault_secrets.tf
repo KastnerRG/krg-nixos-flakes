2cc74ff03958cbf8459d26d16a4d8c98af267622
Sean Perry <shperry@ucsd.edu>
2026-05-27 18:54:31 -0700

    feat (krg-deploy) add terraform for authentik config

---
+++ terraform/authentik/vault_secrets.tf
@@ -0,0 +1,41 @@
+# Write generated OIDC client secrets into vault so downstream services
+# (Grafana compose, Outline secrets.env) can read them without manual copy-paste.
+# The grafana/ Terraform workspace reads grafana-oidc from here.
+
+resource "vault_kv_secret_v2" "grafana_oidc" {
+  mount = "secret"
+  name  = "krg-prod/grafana-oidc"
+  data_json = jsonencode({
+    client_id     = authentik_provider_oauth2.grafana.client_id
+    client_secret = authentik_provider_oauth2.grafana.client_secret
+    issuer_url    = "${var.authentik_url}/application/o/grafana/"
+  })
+}
+
+resource "vault_kv_secret_v2" "outline_oidc" {
+  mount = "secret"
+  name  = "krg-prod/outline-oidc"
+  data_json = jsonencode({
+    client_id     = authentik_provider_oauth2.outline.client_id
+    client_secret = authentik_provider_oauth2.outline.client_secret
+    issuer_url    = "${var.authentik_url}/application/o/outline/"
+  })
+}
+
+resource "vault_kv_secret_v2" "mlflow_oidc" {
+  mount = "secret"
+  name  = "krg-prod/mlflow-oidc"
+  data_json = jsonencode({
+    client_id     = authentik_provider_oauth2.mlflow.client_id
+    client_secret = authentik_provider_oauth2.mlflow.client_secret
+    issuer_url    = "${var.authentik_url}/application/o/mlflow/"
+  })
+}
+
+resource "vault_kv_secret_v2" "outpost_token" {
+  mount = "secret"
+  name  = "krg-prod/authentik-outpost-token"
+  data_json = jsonencode({
+    token = authentik_outpost.proxy.token
+  })
+}
